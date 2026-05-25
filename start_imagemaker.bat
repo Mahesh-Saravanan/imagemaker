e37a2382349d67797598ab5d04fb67d02f2f9dee
Mahesh <mahesh.saravanan.3493@gmail.com>
2026-05-25 18:12:47 +0530

    Update batch script to open browser in full screen without headers

--- start_imagemaker.bat
+++ start_imagemaker.bat
@@ -8,8 +8,8 @@ echo ==========================================
 :: Change directory to the folder where this .bat file is located
 cd /d "%~dp0"
 
-:: Start a background process that waits 4 seconds and then opens the browser
-start cmd /c "timeout /t 4 /nobreak > nul & start http://localhost:3000"
+:: Start a background process that waits 4 seconds and then opens the browser in full screen without headers
+start cmd /c "timeout /t 4 /nobreak > nul & start chrome --start-fullscreen --app=http://localhost:3000 || start msedge --kiosk http://localhost:3000 || start http://localhost:3000"
 
 :: Run the Next.js development server in this window
 npm run dev
