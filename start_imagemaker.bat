4e16c489328f7e2bf2feb60fd8ce32ea556ff6c0
Mahesh <mahesh.saravanan.3493@gmail.com>
2026-05-25 18:35:40 +0530

    Update start script to use Chrome kiosk mode

--- start_imagemaker.bat
+++ start_imagemaker.bat
@@ -8,8 +8,8 @@ echo ==========================================
 :: Change directory to the absolute path of the project on the Windows machine
 cd /d "C:\Users\SKTC\Documents\imagemaker"
 
-:: Start a background process that waits 4 seconds and then opens the browser in full screen without headers
-start cmd /c "timeout /t 4 /nobreak > nul & start chrome --start-fullscreen --app=http://localhost:3000 || start msedge --kiosk http://localhost:3000 || start http://localhost:3000"
+:: Start a background process that waits 4 seconds and then opens the browser in true full screen (no title bar)
+start cmd /c "timeout /t 4 /nobreak > nul & start chrome --kiosk http://localhost:3000 || start msedge --kiosk http://localhost:3000 || start http://localhost:3000"
 
 :: Run the Next.js development server in this window
 npm run dev
